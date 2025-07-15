0e39dd7766a4e1f395254a94fd3d91f91d6b22c7
sorengtotan <sorengtotan@gmail.com>
2025-07-15 15:30:59 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bocil Tetek';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Tetek Smp Mama Live Viral Colmek Sma Montok Tante China';
+export const url = 'https://bociltetek.pages.dev';
+export const terbit = '2025-01-27T00:01:03Z';
+
+export const google = 'jNNOFY1tzeHCcB4Xe72wtIaAul-RDfjCl2E8BlvQNG0';
+export const yandex = 'c4ab21700852ad93'
+
+export const index = '130ec7d3-26a4-469e-acaa-9c585874fdff';
