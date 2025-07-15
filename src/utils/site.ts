2ae398c4e225201f8fd1664b3d95bff683316f62
sorengtotan <sorengtotan@gmail.com>
2025-07-15 15:30:08 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bocil Tetek';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Tetek Smp Mama Live Viral Colmek Sma Montok Tante China';
-export const url = 'https://bociltetek.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
